688f27bb279d44caa4013701796da0e428762006
Abduvosit <57204643+Abduvosit-Bro@users.noreply.github.com>
2023-12-01 19:57:28 +0500

    photo_api.py

--- photo/photo_api.py
+++ photo/photo_api.py
@@ -3,10 +3,9 @@ import os
 photo_router = APIRouter(prefix='/photo', tags=['Фотографии'])
 
 
-# Добавления фото(для аватарки)
+
 @photo_router.post('/add-photo')
 async def add_user_profile_photo(photo_file: UploadFile, user_id: int):
-    # Сохранить локально фото
     with open(f'media/{photo_file.filename}', 'wb') as file:
         user_photo = await photo_file.read()
 
@@ -15,10 +14,8 @@ async def add_user_profile_photo(photo_file: UploadFile, user_id: int):
     return {'message': 'Successfully'}
 
 
-# Изменения фото(для аватарки)
 @photo_router.put('/edit-photo')
 async def edit_user_profile_photo(new_photo_file: UploadFile, user_id: int):
-    # Сохранить локально фото
     with open(f'media/{new_photo_file.filename}', 'wb') as file:
         user_photo = await new_photo_file.read()
 
@@ -27,7 +24,6 @@ async def edit_user_profile_photo(new_photo_file: UploadFile, user_id: int):
     return {'message': 'Successfully edited'}
 
 
-# Удаления фото(для аватарки)
 @photo_router.delete('/delete-photo')
 async def delete_user_profile_photo(user_id: int):
     file_name = (user_id)
