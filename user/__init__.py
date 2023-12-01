00c2db4fca6b2c2a828b7c9d3cdfca7171818502
Abduvosit <57204643+Abduvosit-Bro@users.noreply.github.com>
2023-12-01 19:58:37 +0500

    __init__.py

--- user/__init__.py
+++ user/__init__.py
@@ -1,13 +1,11 @@
 from pydantic import BaseModel
 
 
-# Валидатор для входа в аккаунт
 class LoginUserValidator(BaseModel):
     email: str
     password: str
 
 
-# Валидатор для регистрации
 class RegisterUserValidator(BaseModel):
     name: str
     surname: str
@@ -17,7 +15,6 @@ class RegisterUserValidator(BaseModel):
     password: str
 
 
-# Валидатор  для изменения данных пользователя
 class EditUserValidator(BaseModel):
     user_id: int
     name: str
